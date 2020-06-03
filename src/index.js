3be2aae6b748e0eb6ddafdd2f95f8251325f0303
Brian Park <park-brian@users.noreply.github.com>
2020-06-03 16:40:26 -0400

    remove unneeded components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <pre>To browse components, run "npm run storybook" and navigate to localhost:9009</pre>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
