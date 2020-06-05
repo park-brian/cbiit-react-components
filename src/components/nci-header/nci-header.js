650335460cfdc3b7451b8a231541197e2115862e
Brian Park <park-brian@users.noreply.github.com>
2020-06-05 11:08:39 -0400

    add responsive header

---
+++ src/components/nci-header/nci-header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export function NCIHeader({
+    className = 'p-4',
+    style = {},
+    imageClassName = '',
+    imageStyle = {maxWidth: '80%'},
+    imageProps = {},
+    imageSource = 'assets/images/nci-logo.svg',
+    url = 'https://www.cancer.gov/'
+}) {
+    return <header className={className} style={style}>
+        <a href={url} target="_blank" rel="noopener noreferrer">
+            <img 
+                src={imageSource} 
+                style={imageStyle}
+                className={imageClassName}
+                height="80" 
+                alt="National Cancer Institute Logo" 
+                {...imageProps}
+            />
+        </a>
+    </header>
+    
+}
