2473c174a763889d9a94bd794a3dc50a6302bf2d
Brian Park <park-brian@users.noreply.github.com>
2020-06-08 11:34:57 -0400

    add stories for footer

---
+++ src/stories/9-Footer.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NCIFooter } from '../components/nci-footer/nci-footer';
+import '../components/index.scss';
+
+export default {
+  title: 'NCI Footer',
+  component: NCIFooter,
+};
+
+export function Example() {
+  return <NCIFooter />
+}
+
