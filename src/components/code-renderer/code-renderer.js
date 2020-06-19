15f98aacc9da2e8e8c780e020abd554ae0221d29
Brian Park <park-brian@users.noreply.github.com>
2020-06-19 11:29:42 -0400

    update unindent function

--- src/components/code-renderer/code-renderer.js
+++ src/components/code-renderer/code-renderer.js
@@ -4,8 +4,13 @@ import theme from 'prism-react-renderer/themes/duotoneLight';
 
 export function CodeRenderer({
     code = '',
-    language = 'jsx'
+    language = 'jsx',
+    removeIndentation = false,
 }) {
+    if (removeIndentation) {
+        code = unindent(code)
+    }
+
     return <Highlight {...defaultProps} code={code} language={language} theme={theme}>
         {({ className, style, tokens, getLineProps, getTokenProps }) => (
             <pre className={className} style={style}>
@@ -19,4 +24,20 @@ export function CodeRenderer({
             </pre>
         )}
     </Highlight>
+}
+
+function unindent(code) {
+    // remove leading empty line, and trailing whitespace and split lines
+    let lines = code.replace(/^\s*\n/, '').trimRight().split('\n');
+
+    // determine smallest left indent for all lines
+    let leftIndent = lines
+        .filter(line => !/^\s*$/.test(line)) // ignore empty lines
+        .reduce((smallestIndent, line) => 
+            Math.min(smallestIndent, line.match(/[^\s]/).index), // index of first non-space character
+            Number.MAX_SAFE_INTEGER // default indentation value
+        );
+
+    // join lines, removing left indentation
+    return lines.map(line => line.slice(leftIndent)).join('\n');     
 }
