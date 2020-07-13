5a9ba8e5cbb857dc77e1989134cf373f2e861eb7
Brian Park <park-brian@users.noreply.github.com>
2020-07-13 02:25:12 -0400

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { Button } from './button/button';
+import { ButtonGroup } from './button-group/button-group';
+import { CodeRenderer } from './code-renderer/code-renderer';
+import { Icon } from './icon/icon';
+import { LoadingOverlay } from './loading-overlay/loading-overlay';
+import { Navbar } from './navbar/navbar';
+import { NCIFooter } from './nci-footer/nci-footer';
+import { NCIHeader } from './nci-header/nci-header';
+import { Table } from './table/table';
+import { TreeSelect } from './tree-select/tree-select';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <pre>To browse components, run "npm run storybook" and navigate to localhost:9009</pre>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export {
+    Button,
+    ButtonGroup,
+    CodeRenderer,
+    Icon,
+    LoadingOverlay,
+    Navbar,
+    NCIFooter,
+    NCIHeader,
+    Table,
+    TreeSelect,
+}
